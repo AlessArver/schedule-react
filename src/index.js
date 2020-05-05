cb0edeb0a1a1489faee7be0ad117bc2c73e1abff
Olesya Vyalaya <olesya.vyalaya@arkadium.com>
2020-05-06 01:37:39 +0300

    create store object

--- src/index.js
+++ src/index.js
@@ -2,21 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
-import {state, updateNewTodoText, updateNewSchedule, addSchedule, addTodo, subscribe} from "./redux";
+import store from "./redux";
 import App from './App';
 
 let renderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} addSchedule={addSchedule} addTodo={addTodo} updateNewSchedule={updateNewSchedule} updateNewTodoText={updateNewTodoText} />
+                <App state={state}
+                     addSchedule={store.addSchedule.bind(store)}
+                     addTodo={store.addTodo.bind(store)}
+                     updateNewSchedule={store.updateNewSchedule.bind(store)}
+                     updateNewTodoText={store.updateNewTodoText.bind(store)}/>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
-renderEntireTree(state)
-subscribe(renderEntireTree)
+renderEntireTree(store.getState())
+store.subscribe(renderEntireTree)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,64 +1,65 @@
-let renderEntireTree = () => {
-    console.log("update")
-}
-
-export const state = {
-    schedulesPage: {
-        schedules: [
-            {id: 1, text: "Go to party", date: "01.05.2021"},
-            {id: 2, text: "Go to Museum", date: "01.05.2021"},
-            {id: 3, text: "Go to party", date: "01.05.2021"}
-        ],
-        newScheduleText: "",
-        newScheduleDate: ""
+const store = {
+    _state: {
+        schedulesPage: {
+            schedules: [
+                {id: 1, text: "Go to party", date: "01.05.2021"},
+                {id: 2, text: "Go to Museum", date: "01.05.2021"},
+                {id: 3, text: "Go to party", date: "01.05.2021"}
+            ],
+            newScheduleText: "",
+            newScheduleDate: ""
+        },
+        todosPage: {
+            todos: [
+                {id: 1, text: "Buy Amazon company", isComplete: true},
+                {id: 2, text: "Sing very cool song", isComplete: true},
+                {id: 3, text: "Run 5km", isComplete: false}
+            ],
+            newTodoText: "",
+            newTodoIsComplete: false
+        }
     },
-    todosPage: {
-        todos: [
-            {id: 1, text: "Buy Amazon company", isComplete: true},
-            {id: 2, text: "Sing very cool song", isComplete: true},
-            {id: 3, text: "Run 5km", isComplete: false}
-        ],
-        newTodoText: "",
-        newTodoIsComplete: false
+    getState() {
+        return this._state
+    },
+    _callSubscriber() {
+        console.log("update")
+    },
+    addSchedule() {
+        let newSchedule = {
+            id: 4,
+            text: this._state.schedulesPage.newScheduleText,
+            date: this._state.schedulesPage.newScheduleDate
+        }
+
+        this._state.schedulesPage.schedules.push(newSchedule)
+        this._state.schedulesPage.newScheduleText = ""
+        this._callSubscriber(this._state)
+    },
+    addTodo() {
+        let newTodo = {id: 4, text: this._state.todosPage.newTodoText, isComplete: false}
+
+        this._state.todosPage.todos.push(newTodo)
+        this._state.todosPage.newTodoText = ""
+        this._callSubscriber(this._state)
+    },
+    updateNewSchedule(newText, newDate) {
+        this._state.schedulesPage.newScheduleText = newText
+        this._state.schedulesPage.newScheduleDate = newDate
+        this._callSubscriber(this._state)
+    },
+    updateNewTodoText(newText) {
+        this._state.todosPage.newTodoText = newText
+        this._callSubscriber(this._state)
+    },
+    completeTodo(newIsComplete) {
+        this._state.todosPage.newTodoIsComplete = newIsComplete
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer
     }
 }
-window.state = state
+window.store = store
 
-export const addSchedule = () => {
-    let newSchedule = {
-        id: 4,
-        text: state.schedulesPage.newScheduleText,
-        date: state.schedulesPage.newScheduleDate
-    }
-
-    state.schedulesPage.schedules.push(newSchedule)
-    state.schedulesPage.newScheduleText = ""
-    renderEntireTree(state)
-}
-
-export const addTodo = () => {
-    let newTodo = {id: 4, text: state.todosPage.newTodoText, isComplete: false}
-
-    state.todosPage.todos.push(newTodo)
-    state.todosPage.newTodoText = ""
-    renderEntireTree(state)
-}
-
-export const updateNewSchedule = (newText, newDate) => {
-    state.schedulesPage.newScheduleText = newText
-    state.schedulesPage.newScheduleDate = newDate
-    renderEntireTree(state)
-}
-
-export const updateNewTodoText = (newText) => {
-    state.todosPage.newTodoText = newText
-
-    renderEntireTree(state)
-}
-
-export const completeTodo = (newIsComplete) => {
-    state.todosPage.newTodoIsComplete = newIsComplete
-    renderEntireTree(state)
-}
-
-export const subscribe = (observer) => renderEntireTree = observer
+export default store
