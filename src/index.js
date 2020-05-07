e76e150f5d96870258f73997ca359c99a7fc397f
Olesya Vyalaya <olesya.vyalaya@arkadium.com>
2020-05-08 02:07:58 +0300

    add redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
-import store from "./redux";
+import store from "./redux/myRedux";
 import App from './App';
 
 let renderEntireTree = (state) => {
@@ -16,7 +16,7 @@ let renderEntireTree = (state) => {
     );
 }
 renderEntireTree(store.getState())
-store.subscribe(renderEntireTree)
+store.subscribe(() => renderEntireTree(store.getState()))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
