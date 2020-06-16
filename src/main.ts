601b642fcbc11c607de756ac94d6abea996aec70
Olesya Vyalaya <olesya.vyalaya@arkadium.com>
2020-06-17 01:04:23 +0300

    change package in client

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.use(cookieParser())
-
+//
 if (process.env.NODE_ENV === 'production') {
   //app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')))
   app.get('*', (req: express.Request, res: express.Response) => {
